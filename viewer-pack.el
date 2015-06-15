2540a39c27b2ba9e6a7d502c18a7cf55c0cb91c8
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-06-15 11:15:46 +0200

    Add some default setup (continuous mode to visualize pdf)

---
+++ viewer-pack.el
@@ -0,0 +1,13 @@
+;;; viewer-pack.el --- Different viewers setup       -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package doc-view
+  :init
+  (custom-set-variables
+   '(doc-view-continuous t)))
+
+(provide 'viewer-pack)
+;;; viewer-pack ends here
